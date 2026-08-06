fe7191b3ff875a5b9043229ec2081e662978ecb4
RHARIJIT <auroville1928@gmail.com>
2026-08-06 10:16:41 +0530

    Initial FastAPI backend setup

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from src.utils.db import Base,engine
+from src.tasks.models import TaskModel
+from src.user.router import user_routes
+
+from src.tasks.router import task_routes
+
+Base.metadata.create_all(engine)
+
+app=FastAPI(title="This is the TaskMaster project")
+
+# importing the task endpoints
+app.include_router(task_routes)
+
+# importing the user endpoints
+app.include_router(user_routes)
---
+++ src/user/controller.py
@@ -0,0 +1,38 @@
+from fastapi import HTTPException
+from src.user.dtos import UserSchema
+from sqlalchemy.orm import Session
+from src.user.models import UserModel
+from pwdlib import PasswordHash
+
+# creating hasheed password
+password_hash = PasswordHash.recommended()
+
+
+def get_password_hash(password):
+    return password_hash.hash(password)
+
+
+def register(body: UserSchema, db: Session):
+    print(body)
+    is_user = db.query(UserModel).filter(UserModel.username == body.username).first()
+    if is_user:
+        raise HTTPException(400, detail="Username already exist..")
+
+    is_email = db.query(UserModel).filter(UserModel.email == body.email).first()
+    if is_email:
+        raise HTTPException(400, detail="Email already exists...")
+
+    hash_password = get_password_hash(body.password)
+
+    new_user = UserModel(
+        name=body.name,
+        username=body.username,
+        hash_password=hash_password,
+        email=body.email
+    )
+    
+    db.add(new_user)
+    db.commit
+    db.refresh(new_user)
+    
+    return new_user
